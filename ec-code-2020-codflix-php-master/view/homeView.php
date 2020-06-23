d1e6fa1cbf87fca81d62322ff95fa27f25d1eafa
Hugo <hugo.pavard92@gmail.com>
2020-06-23 16:57:10 +0200

    Contact button

--- ec-code-2020-codflix-php-master/view/homeView.php
+++ ec-code-2020-codflix-php-master/view/homeView.php
@@ -15,6 +15,13 @@
                 <div class="col-md-6"><a href="index.php?action=login" class="btn btn-block bg-red">Connexion</a></div>
                 <div class="col-md-6"><a href="index.php?action=signup" class="btn btn-block bg-blue">Inscription</a></div>
             </div>
+            <!-- 
+                Adding another row button container with a contact button on the inside at the bottom of the home page.
+                I used the same existing css as the title to make it placed in the center of the row.
+            -->
+            <div class="row btn-container">
+                <div class="col-md-12"><a href="contact.php" class="btn btn-block">Nous contacter</a></div>
+            </div>
         </div>
     </div>
 </div>
