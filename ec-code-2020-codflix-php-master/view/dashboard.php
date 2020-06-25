26a11c875b4a0264fe2ce5e58964f272a0b30071
Hugo <hugo.pavard92@gmail.com>
2020-06-25 12:15:41 +0200

    3 movies added to database

--- ec-code-2020-codflix-php-master/view/dashboard.php
+++ ec-code-2020-codflix-php-master/view/dashboard.php
@@ -8,22 +8,21 @@ unset($_SESSION['role']);
   <head>
     <meta charset="utf-8" />
     <title>Cod'Flix</title>
-
     <link href="public/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <link href="public/lib/font-awesome/css/all.min.css" rel="stylesheet" />
-
     <link href="public/css/partials/partials.css" rel="stylesheet" />
     <link href="public/css/layout/layout.css" rel="stylesheet" />
   </head>
 
-
   <body>
+
     <div class="wrapper d-flex align-items-stretch">
       <nav id="sidebar">
         <h2 class="title">Bienvenue</h2>
         <div class="sidebar-menu">
           <ul>
-            <li class="active"><a href="/CodFlix/">Médias</a></li>
+            <!-- Adding new links to the media list page and to the contact page -->
+            <li><a href="view/mediaListView.php">Médias</a></li>
             <li><a href="contact.php">Nous contacter</a></li>
             <li><a href="index.php?action=logout">Me déconnecter</a></li>
           </ul>
@@ -50,8 +49,8 @@ unset($_SESSION['role']);
 
     <script src="public/lib/jquery/js/jquery-3.5.0.min"></script>
     <script src="public/lib/bootstrap/js/bootstrap.min.js"></script>
-
     <script src="public/js/script.js"></script>
+    
   </body>
 
 </html>
